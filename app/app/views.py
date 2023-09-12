053187e1e4434b22e4fea5e5b52005eec23fcbf0
Alex Müller <alexarnimueller@protonmail.com>
2023-09-12 19:49:25 +0200

    :scream_cat: :computer: hacked

--- app/app/views.py
+++ app/app/views.py
@@ -1,5 +1,5 @@
-from flask import Blueprint
-from flask import render_template
+import os
+from flask import Blueprint, render_template, request, session, redirect, url_for, flash
 from auth import login_required
 
 bp = Blueprint("views", __name__)
@@ -14,3 +14,34 @@ def index():
 @login_required
 def infos():
     return render_template("infos.html")
+
+
+@bp.route("/hack", methods=("GET", "POST"))
+def hack():
+    if request.method == "POST":
+        password = request.form["password"]
+        if password == "hack":
+            session["hacked"] = True
+            return redirect(url_for("views.valmira"))
+    return render_template("hack.html")
+
+
+@bp.route("/valmira", methods=("GET", "POST"))
+def valmira():
+    if session.get("hacked", False):
+        if request.method == "POST":
+            password = request.form["handy"]
+            if os.getenv("VALMIRA_HANDY") == password:
+                session["valmira"] = True
+                return redirect(url_for("views.ortung"))
+
+            flash("Nummer unbekannt!", "danger")
+        return render_template("valmira.html")
+    return redirect(url_for("views.hack"))
+
+
+@bp.route("/ortung", methods=("GET",))
+def ortung():
+    if session.get("valmira", False):
+        return render_template("ortung.html")
+    return render_template("valmira.html")
